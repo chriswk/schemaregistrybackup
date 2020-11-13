d183fa091e3b5f1beff517d06895fec8e7df0d3a
Christopher Kolstad <git@chriswk.no>
2020-11-13 15:09:13 +0100

    Added test for new version of schema

--- src/test/kotlin/com/chriswk/kafka/schemaregistry/BackupTest.kt
+++ src/test/kotlin/com/chriswk/kafka/schemaregistry/BackupTest.kt
@@ -1,6 +1,7 @@
 package com.chriswk.kafka.schemaregistry
 
 import io.confluent.kafka.serializers.KafkaAvroDeserializer
+import org.apache.avro.SchemaBuilder
 import org.apache.avro.generic.GenericData
 import org.apache.avro.generic.GenericRecord
 import org.apache.kafka.clients.producer.ProducerRecord
@@ -89,19 +90,13 @@ class BackupTest {
 
     @Test
     fun `schema submitted to schema registry should be reflected in backup`() {
-        val schema = """
-            {"namespace": "example.avro",
-             "type": "record",
-             "name": "User",
-             "fields": [
-                 {"name": "name", "type": "string"},
-                 {"name": "favorite_number",  "type": ["int", "null"]},
-                 {"name": "favorite_color", "type": ["string", "null"]}
-             ]
-            }
-        """
-        val avroSchema = org.apache.avro.Schema.Parser().parse(schema)
-        val record = GenericData.Record(avroSchema)
+        val schema = SchemaBuilder.builder("example.avro")
+            .record("User").fields()
+            .requiredString("name")
+            .optionalInt("favorite_number")
+            .optionalString("favorite_color")
+            .endRecord()
+        val record = GenericData.Record(schema)
         record.put("name", "Willy")
         record.put("favorite_number", 51)
         record.put("favorite_color", "Blue")
@@ -115,6 +110,44 @@ class BackupTest {
             record2.put("favorite_number", 51)
             record2.put("favorite_color", "Blue")
         }
+    }
+
+    @Test
+    fun `Update to schema should also be reflected in backup`() {
+        val schema = SchemaBuilder.builder("example.avro")
+            .record("User").fields()
+            .requiredString("name")
+            .optionalInt("favorite_number")
+            .optionalString("favorite_color")
+            .endRecord()
+
+        val record = GenericData.Record(schema)
+        record.put("name", "Willy")
+        record.put("favorite_number", 51)
+        record.put("favorite_color", "Blue")
+        kafkaTemplate.send(ProducerRecord("test-topic", "testkey", record)).get()
+        val updatedSchema = """
+                        {"namespace": "example.avro",
+             "type": "record",
+             "name": "User",
+             "fields": [
+                 {"name": "name", "type": "string"},
+                 {"name": "favorite_number",  "type": ["int", "null"]},
+                 {"name": "favorite_color", "type": ["string", "null"]},
+                 {"name": "favorite_movie", "type": ["string", "null"], "default": "null"}
+             ]
+            }
+        """.trimIndent()
+        val parser = org.apache.avro.Schema.Parser()
+        val updated = parser.parse(updatedSchema)
+        val record2 = GenericData.Record(updated)
+        record2.put("name", "Willy")
+        record2.put("favorite_number", 51)
+        record2.put("favorite_color", "Blue")
+        record2.put("favorite_movie", "The Big Lebowski")
+        kafkaTemplate.send(ProducerRecord("test-topic", "testkey2", record2)).get()
+        val schemasForSubject = schemaRepository.findAllBySubject("test-topic-value")
+        assertThat(schemasForSubject).hasSize(2)
     }
 
 }
